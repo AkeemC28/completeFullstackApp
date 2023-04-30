42b35591506dcf37100953e283f9ed2312cdfdb5
Akeem Chambers <chambersakeem@gmail.com>
2023-04-30 11:55:16 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://chambersakeem:wds21ThEMiIqViZE@212121.v6j7yvm.mongodb.net/demo?retryWrites=true&w=majority', 
+    'dbName': 'demo'
+};
